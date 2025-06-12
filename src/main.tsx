d9fb0341deb6cdab36165aa82e2883792001dc4a
Andreja Zarkovic <andreja.zarkovic@impactcommerce.se>
2025-06-12 09:27:26 +0200

    wrap app with theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ReportProvider } from "./context/ReportContext.tsx";
+import { ReportProvider } from "./context/report/ReportContext.tsx";
+import { ThemeModeProvider } from "./context/theme/ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ReportProvider>
-      <App />
-    </ReportProvider>
+    <ThemeModeProvider>
+      <ReportProvider>
+        <App />
+      </ReportProvider>
+    </ThemeModeProvider>
   </StrictMode>
 );
