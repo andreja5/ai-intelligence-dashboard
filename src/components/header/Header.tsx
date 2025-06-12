9d62782fe5d539a4e1ff7f9744c74c21ece6b177
Andreja Zarkovic <andreja.zarkovic@impactcommerce.se>
2025-06-12 09:16:54 +0200

    create a header with app name, search, create new report button and light/dark mode button switch

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,69 @@
+import type { ChangeEvent } from "react";
+import {
+  Box,
+  Typography,
+  TextField,
+  Button,
+  Stack,
+  IconButton,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import { useThemeMode } from "../../context/theme/ThemeContext";
+
+interface Props {
+  onSearchChange: (value: string) => void;
+  onCreateClick: () => void;
+}
+
+export const Header = ({ onSearchChange, onCreateClick }: Props) => {
+  const { mode, toggleMode } = useThemeMode();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onSearchChange(e.target.value);
+  };
+
+  return (
+    <Box
+      sx={{
+        mb: 3,
+        gap: 2,
+        display: "flex",
+        alignItems: { sm: "center" },
+        justifyContent: "space-between",
+        flexDirection: { xs: "column", sm: "row" },
+      }}
+    >
+      <Typography variant="h4">Intelligence Dashboard</Typography>
+
+      <Stack
+        spacing={2}
+        direction="row"
+        width={{ xs: "100%", md: "auto" }}
+        justifyContent={{ xs: "center", lg: "left" }}
+      >
+        <TextField
+          focused
+          size="small"
+          color="primary"
+          variant="outlined"
+          label="Search Reports"
+          onChange={handleInputChange}
+        />
+
+        <Button
+          variant="contained"
+          startIcon={<AddIcon />}
+          onClick={onCreateClick}
+        >
+          New Report
+        </Button>
+
+        <IconButton onClick={toggleMode}>
+          {mode === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+        </IconButton>
+      </Stack>
+    </Box>
+  );
+};
